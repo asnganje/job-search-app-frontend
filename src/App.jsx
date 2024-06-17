9a40afa03f89bb06ab01fdcb000d903584ef9467
asnganje <abd.nganje@gmail.com>
2024-06-17 20:33:26 +0300

    Add basic app routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,19 @@
-
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
 function App() {
-  return (<h2>Job Search App</h2>)
+
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <h2>Home</h2>
+    },
+    {
+      path: '/about',
+      element: <h2>About</h2>
+    }
+  ])
+  return (
+    <RouterProvider router={router}/>
+)
 }
 
 export default App
