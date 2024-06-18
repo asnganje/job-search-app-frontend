f32703ec84efc3493b5c6ac99c68aba5123d6962
asnganje <abd.nganje@gmail.com>
2024-06-18 09:05:11 +0300

    Modify error page content

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,13 +1,24 @@
 import { useRouteError, Link } from "react-router-dom";
-
+import notFound from '../assets/not-found.svg'
 const Error = () => {
   const error = useRouteError()
-  console.log(error);
+  if (error.status === 404) {
+    return(<section className="error-wrapper">
+      <div>
+        <img src={notFound} alt="not-found" className="error-img"/>
+        <h3>Ohh! page not found</h3>
+        <p>We can't seem to find the page you are looking for</p>
+        <Link to='/dashboard' className="a">back home</Link>
+      </div>
+    </section>
+    )
+  }
   return(
-    <div>
-      <h2>Error page</h2>
-      <Link to = "/">Back home</Link>
-    </div>
+    <section className="error-wrapper">
+      <div>
+        <h3>Something went wrong</h3>
+      </div>
+    </section>
   )
 }
 
