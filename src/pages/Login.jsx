caeb3e97971c68a52c5586ed94127fc51a5cd5db
asnganje <abd.nganje@gmail.com>
2024-06-18 21:05:58 +0300

    Modify login content

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,20 @@
 import { Link } from "react-router-dom";
+import { FormRow, Logo } from "../components";
+
 const Login = () => {
   return(
-    <div>
-      <h2>Login Page</h2>
-      <Link to="/register">Register page</Link>
-    </div>
+    <section className="form-wrapper">
+      <form className="form">
+        <Logo className = "logo"/>
+        <h4>Login</h4>
+        <FormRow type="email" name="email"/>
+        <FormRow type="password" name="password"/>
+        <button type="submit" className="btn btn-block">submit</button>
+          <p>Not a member yet?
+            <Link to="/register" className="member-btn">Register</Link>
+          </p>
+      </form>
+    </section>
   )
 }
 
