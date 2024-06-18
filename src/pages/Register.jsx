fb01a2519c7f0e248e298c94a31865ecfe20260c
asnganje <abd.nganje@gmail.com>
2024-06-18 21:06:08 +0300

    Modify register content

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,10 +1,22 @@
 import { Link } from "react-router-dom";
+import { FormRow, Logo } from "../components";
 const Register = () => {
   return(
-    <div>
-      <h2>Register Page</h2>
-      <Link to="/login">Login page</Link>
-    </div>
+    <section className="form-wrapper">
+      <form className="form">
+          <Logo className = "logo"/>
+          <h4>Register</h4>
+            <FormRow type="text" name="name"/>
+            <FormRow type="text" labelText="Last Name"/>
+            <FormRow type="text" name="Location"/>
+            <FormRow type="email" name="email"/>
+            <FormRow type="password" name="password"/>
+          <button type="submit" className="btn btn-block">submit</button>
+          <p>Already a member?
+            <Link to="/login" className="member-btn">Login</Link>
+          </p>
+      </form>
+    </section>
   )
 }
 
