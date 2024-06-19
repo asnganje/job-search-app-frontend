2f2b0d07bf6945b3b66d667bd2674b8ebbbd5014
asnganje <abd.nganje@gmail.com>
2024-06-19 19:36:41 +0300

    Add navbar content

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,22 @@
+import {FaAlignLeft } from "react-icons/fa"
+import Logo from './Logo'
+import { useDashboardContext } from "../pages/DashboardLayout"
+
 const Navbar = () => {
+  const {toggleSidebar} = useDashboardContext()
   return(
-    <div>
-      Nav sidebar
-    </div>
+    <nav className="navbar-wrapper">
+      <div className="nav-center">
+        <button onClick={toggleSidebar} className="toggle-btn">
+          <FaAlignLeft />
+        </button>
+      <div className="logo-control">
+        <Logo className="logo"/>
+        <h4 className="logo-text">dashboard</h4>
+      </div>
+      <div className="btn-container">toggle/logout</div>
+      </div>
+    </nav>
   )
 }
 
