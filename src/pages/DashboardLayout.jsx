91d4307f216b143e7c3933a03b022c61e70a983a
asnganje <abd.nganje@gmail.com>
2024-06-19 15:08:44 +0300

    Add state to dashboard

--- src/pages/DashboardLayout.jsx
+++ src/pages/DashboardLayout.jsx
@@ -1,9 +1,46 @@
+import { Outlet } from "react-router-dom";
+import { BigSidebar, Navbar, SmallSidebar } from "../components";
+import { createContext, useContext, useState } from "react";
+
+const DashboardContext = createContext()
+
 const DashboardLayout = () => {
+  const user = {name: 'abdul'}
+
+  const [getUser, setGetUser] = useState(user)
+  const [showSidebar, setShowSidebar] = useState(false)
+
+  const toggleSidebar = () => {
+    setShowSidebar(!showSidebar)
+  }
+
+
+  const logoutUser = () => {
+    const updatedUser = user.name = null    
+    setGetUser(updatedUser)
+  }
+
   return(
-    <div>
-      DashboardLayout Page
-    </div>
+    <DashboardContext.Provider value={{getUser, showSidebar, toggleSidebar, logoutUser}}>
+      <section className="dashboard-wrapper">
+        <main className="dashboard">
+          <SmallSidebar />
+          <BigSidebar />
+          <div>
+            <Navbar />
+            <div className="dashboard-page">
+              <Outlet />
+            </div>
+          </div>
+        </main>
+      </section>
+    </DashboardContext.Provider>
   )
 }
+
+export const useDashboardContext = () => {
+  return useContext(DashboardContext)
+}
+
 
 export default DashboardLayout;
