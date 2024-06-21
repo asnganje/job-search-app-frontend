f938227b8806617bf8ec57f01825cfb1cce3910e
asnganje <abd.nganje@gmail.com>
2024-06-21 07:19:39 +0300

    modify form row select component

---
+++ src/components/FormRowSelect.jsx
@@ -0,0 +1,15 @@
+const FormRowSelect = ({name, labelText, list, defaultValue=''}) => {
+  return(
+    <div className="form-row">
+              <label htmlFor={name} className="form-label">{labelText || name}</label>
+              <select name={name} id={name} className="form-select" defaultValue={defaultValue}>
+
+                {
+                  list.map((item)=><option key={item} value={item}>{item}</option>)
+                }
+              </select>
+            </div>
+  )
+}
+
+export default FormRowSelect;
